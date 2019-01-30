5de29794acbf7103aee308b56f3f354cae6826c1
bjfansr <416261653ddf991da9372cf3f039c2ea7720222f>
2019-01-30 18:47:50 +0800

    添加路由和查询

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+// path：与浏览器地址栏中的URL匹配的字符串。
+// component：导航到此路由时路由器应创建的组件。
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: 'heroes', component: HeroesComponent}
+];
+
+@NgModule({
+  // 初始化路由器并开始监听浏览器位置更改
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
